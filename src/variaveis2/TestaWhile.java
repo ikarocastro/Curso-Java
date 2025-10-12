ddbc665bfc5c05c28bfca8d62e38b55ffc7f586e
Your Name <nunesikaro@gmail.com>
2025-10-11 21:14:40 -0300

    add TestaWhile

---
+++ src/variaveis2/TestaWhile.java
@@ -0,0 +1,19 @@
+package variaveis2;
+
+public class TestaWhile {
+    public static void main(String[] args) {
+
+            int contador = 0;       //Variaveis locais não tem valor padrão tem que iniciar um valor
+
+            while (contador <= 10){
+                System.out.println(contador);
+                contador += 1;
+                //Contador = Contador + 1;
+            }
+        System.out.println(contador);
+    }
+
+}
+
+
+//while em portugues "enquanto tal condição acontecer execute tal tarefa!"
